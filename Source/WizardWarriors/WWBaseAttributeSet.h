5fd99e391295a419688e3e0819af063cacb9f5b1
aaronjansen15 <37312842+aaronjansen15@users.noreply.github.com>
2018-12-28 16:43:18 -0600

    Initial commit

---
+++ Source/WizardWarriors/WWBaseAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "WWBaseAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIZARDWARRIORS_API UWWBaseAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
